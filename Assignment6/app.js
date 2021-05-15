551f463e783e2bb37c048f57bfc9e5677aa9736c
Sajan Kumar <kumarsajan1304@gmail.com>
2021-05-15 16:46:11 +0530

    Assignment 6/ Todo PP

---
+++ Assignment6/app.js
@@ -0,0 +1,53 @@
+// const btn = document.getElementById("btn");
+// let todo = document.getElementById("todo");
+
+let inputText = document.querySelector('input');
+const btn = document.getElementById('btn');
+const ul = document.querySelector('ul');
+
+
+btn.addEventListener("click", todo)
+
+
+
+function todo(){
+    if (inputText.value.length === 0) {
+        return false;
+    }
+    else {
+        const todoItem = inputText.value;
+
+        let li = document.createElement('li');
+        li.innerText = todoItem;
+        ul.appendChild(li);
+        inputText.value = "";
+
+        var dBtn = document.createElement("button");
+        dBtn.appendChild(document.createTextNode("X"));
+        li.appendChild(dBtn);
+        dBtn.addEventListener("click", deleteListItem);
+
+        function deleteListItem() {
+              li.remove();
+        }
+        
+        var edit = document.createElement("button");
+        edit.appendChild(document.createTextNode("Edit"));
+        li.appendChild(edit);
+        edit.addEventListener("click", editLi);
+
+        function editLi() {
+            let p = prompt("Edit Your Entry");
+            li.innerText = p;
+            li.appendChild(dBtn);
+            li.appendChild(edit);
+
+        }
+    }
+}
+
+
+
+
+
+
